74d599f4d4449143e8a4e5670aacabc029933181
blind <blindjh@gmail.com>
2018-04-07 13:16:32 +0900

    Revert 1c6e92d

--- src/index.js
+++ src/index.js
@@ -1,53 +1,8 @@
-<<<<<<< HEAD
-import ReactDOM from 'react-dom';
-import promiseFinally from 'promise.prototype.finally';
-import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
-import {useStrict} from 'mobx';
-import {Provider} from 'mobx-react';
-
-import App from './components/App';
-
-import articlesStore from './stores/articlesStore';
-import commentsStore from './stores/commentsStore';
-import authStore from './stores/authStore';
-import commonStore from './stores/commonStore';
-import editorStore from './stores/editorStore';
-import userStore from './stores/userStore';
-import profileStore from './stores/profileStore';
-import walletsStore from './stores/walletsStore';
-
-const stores = {
-    articlesStore,
-    commentsStore,
-    authStore,
-    commonStore,
-    editorStore,
-    userStore,
-    profileStore,
-    walletsStore,
-};
-
-// For easier debugging
-window._____APP_STATE_____ = stores;
-
-promiseFinally.shim();
-useStrict(true);
-
-ReactDOM.render((
-    <Provider {...stores}>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    </Provider>
-), document.getElementById('root'));
-=======
 // make bluebird default Promise
 Promise = require('bluebird'); // eslint-disable-line no-global-assign
 const { port, env } = require('./config/vars');
 const app = require('./config/express');
 const mongoose = require('./config/mongoose');
-
 
 // open mongoose connection
 mongoose.connect();
@@ -60,4 +15,3 @@ app.listen(port, () => console.info(`server started on port ${port} (${env})`));
 * @public
 */
 module.exports = app;
->>>>>>> 1712f464c18fb0ca26c2bdc956f3cf2e9ad0a4b3
