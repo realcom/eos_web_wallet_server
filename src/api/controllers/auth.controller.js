e706b5d2f4ddb084d436d28dbe1fced2b40cb4fd
blind <blindjh@gmail.com>
2018-04-08 01:15:56 +0900

    before eslint

--- src/api/controllers/auth.controller.js
+++ src/api/controllers/auth.controller.js
@@ -3,6 +3,8 @@ const User = require('../models/user.model');
 const RefreshToken = require('../models/refreshToken.model');
 const moment = require('moment-timezone');
 const { jwtExpirationInterval } = require('../../config/vars');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 /**
 * Returns a formated object with tokens
@@ -23,9 +25,39 @@ function generateTokenResponse(user, accessToken) {
  */
 exports.register = async (req, res, next) => {
   try {
-    const user = await (new User(req.body)).save();
-    const userTransformed = user.transform();
-    const token = generateTokenResponse(user, user.token());
+    // create access key
+
+
+
+    // console.log(`${process.env.CLEOS_EXEC} create key`);
+    // console.log(stderr);
+    // console.log(stdout);
+
+    // const user = await (new User(req.body)).save();
+    // const userTransformed = user.transform();
+    // const token = generateTokenResponse(user, user.token());
+
+    const keyRegex = /Private key: ([a-zA-Z0-9_]*)(\s*)Public key: ([a-zA-Z0-9_]*)/g;
+    console.log(keyRegex)
+    var { stdout, stderr } = await exec(`${process.env.CLEOS_EXEC} create key`);
+    console.log(stdout);
+    console.log(keyRegex.exec(stdout));
+    keyRegex.exec('abc')
+    console.log(abc)
+    const [a, owernerPrivate, b, ownerPublic] = keyRegex.exec(stdout);
+    console.log(ownerPublic);
+    var { stdout, stderr } = await exec(`${process.env.CLEOS_EXEC} create key`);
+    const [c, activePrivate, d, activePublic] = keyRegex.exec(stdout);
+    console.log(ownerPublic)
+    console.log(activePublic)
+    var { stdout, stderr } = await exec(`${process.env.CLEOS_EXEC} create account ${ownerPublic} ${activePublic}`);
+    console.log(stdout);
+    console.log(stderr);
+    return next(error);
+
+
+
+
     res.status(httpStatus.CREATED);
     return res.json({ token, user: userTransformed });
   } catch (error) {
