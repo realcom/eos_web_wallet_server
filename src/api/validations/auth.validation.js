2d7a612741285d8f61ef3d94b40a68fc4833d6e9
blind <blindjh@gmail.com>
2018-06-07 17:07:23 +0900

    account Name validation

--- src/api/validations/auth.validation.js
+++ src/api/validations/auth.validation.js
@@ -6,7 +6,7 @@ module.exports = {
     body: {
       email: Joi.string().email().required(),
       password: Joi.string().required().min(6).max(128),
-      account: Joi.string().required().min(4).max(13),
+      account: Joi.string().required().min(2).max(13),
     },
   },
 
